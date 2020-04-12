31d4ab9b3ad0dd3da0d4bf361324dc06535b29c4
Jan <jan.svehla@velux.com>
2020-04-12 10:20:38 +0200

    edited virtualenvy

---
+++ setupServer.bat
@@ -0,0 +1,6 @@
+REM This line is not required in visual studio code. It runs env automaticly
+REM c:\Programming\ShoppingApp\sopping_env\Scripts\activate.bat 
+cd C:\Programming\ShoppingApp
+set FLASK_APP=flaskShoppingApp.py
+cd C:\Programming\ShoppingApp
+flask run
